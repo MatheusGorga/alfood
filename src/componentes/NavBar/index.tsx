6d85a0f48698317041ae9d7b7d194f241252c307
MatheusGorga <matheusgobar@gmail.com>
2023-08-22 18:27:54 -0300

    implements table mui in project

--- src/componentes/NavBar/index.tsx
+++ src/componentes/NavBar/index.tsx
@@ -10,6 +10,9 @@ const NavBar = () => {
       <li>
         <Link to="/restaurantes">Restaurantes</Link>
       </li>
+      <li>
+        <Link to="/admin/restaurantes">Administração</Link>
+      </li>
     </ul>
   </nav>)
 }
